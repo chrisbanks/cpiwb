7af79283f4fcd9d3a3f3bafcadadec8ce226e76d
Chris Banks <dev@cbcsystems.co.uk>
2011-09-06 16:57:59 +0100

    Suppress warnings about pattern guards.

--- CpiSemantics.hs
+++ CpiSemantics.hs
@@ -15,6 +15,8 @@
 --     You should have received a copy of the GNU General Public License
 --     along with CPiWB.  If not, see <http://www.gnu.org/licenses/>.
 
+{-# OPTIONS_GHC -XPatternGuards #-}
+
 module CpiSemantics where
 
 import qualified Data.List as L
