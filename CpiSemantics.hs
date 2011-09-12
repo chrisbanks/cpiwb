36be148357c3f1b9f7498fe6a8141722abcea809
Chris Banks <dev@cbcsystems.co.uk>
2011-09-12 13:04:01 +0100

    Finalised normal form for concretions.

--- CpiSemantics.hs
+++ CpiSemantics.hs
@@ -118,7 +118,20 @@ instance Nf Concretion where
                 where
                   f (Par ss) = ss 
                   f s = [s]
-    --nf (ConcNew net ) = 
+    nf (ConcNew net@(AffNet ns) (ConcNew net'@(AffNet ns') c))
+        | net##net' && not(net#<c || net'#<c)
+            = nf (ConcNew (net `netUnion` net') c)
+        | net#<c
+            = nf (ConcNew net' c)
+        | net'#<c
+            = nf (ConcNew net c)
+    nf (ConcNew net@(AffNet ns) (ConcPar c ss))
+        | net#<c       = ConcPar (nf c) [(nf(New net (Par ss)))]
+        | net#(Par ss) = ConcPar (nf (ConcNew net c)) [(nf (Par ss))]
+        | otherwise    = ConcNew (AffNet (L.sort ns)) (nf c)
+    nf (ConcNew net@(AffNet ns) c)
+        | net#<c    = nf c
+        | otherwise = ConcNew (AffNet (L.sort ns)) (nf c)
 
 -- Get the Multi-Transition System for a Process:
 processMTS :: [Definition] -> Process -> MTS
