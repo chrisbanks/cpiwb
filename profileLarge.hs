8a0bfc08330905c036d93761c205ab115430a93d
Chris Banks <dev@cbcsystems.co.uk>
2012-09-21 09:36:08 +0100

    Removed old profiling scripts

--- profileLarge.hs
+++
@@ -1,9 +0,0 @@
-import CpiTest
-import CpiLogic
-import CpiLib
-
-main = do env <- tEnv "models/mapk.cpi"
-          let pi = tProc env "MAPK"
-          print $ modelCheck env pi ((0,25),250) prop
-
-prop = Nec (ValLT (Conc (Def "Ras" ["ras"])) (R 3.0))
--- profileSmall.hs
+++
@@ -1,6 +0,0 @@
-import CpiTest
-import CpiLogic
-
-main = do env <- tEnv "models/testGT.cpi"
-          let pi = tProc env "Pi"
-          print $ modelCheck env pi ((0,25),250) tF3
